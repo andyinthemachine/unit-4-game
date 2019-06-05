c8c42a0294fc3b3e08e922dfad715a626b6b5f24
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-06-04 20:26:12 -0600

    added event delegation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,122 +1,75 @@
 
 
-// // non-object version js
 
-// var wins = 0;
-// var board = "";
-// var guesses_left = 9;
-// var guesses = "";
-// var game_words = ["jamaica", "marley", "ganja", "jah", "skankin", "iree", "dred"];
-// var game_won = false;
-// var game_word_index = 0;
-// var num_guesses = 9;
-
-// var wins_display = document.getElementById("wins");
-// var board_display = document.getElementById("board");
-// var guesses_left_display = document.getElementById("guesses_left");
-// var guesses_display = document.getElementById("guesses");
-// var game_word_display = document.getElementById("game_word");
-
-// function update_display()
-// {
-//   wins_display.textContent = wins;
-//   board_display.textContent = board;
-//   guesses_left_display.textContent = guesses_left;
-//   guesses_display.textContent = guesses;
-// }
-
-// // not used function
-
-// function show_winning_word(show)
-// {
-//   if (show)
-//     game_word_display.style.display = 'block';
-//   else
-//     game_word_display.style.display = 'none';
-// }
-
-
-// function increment_game_word_index()
-// {
-//   if (game_word_index === game_words.length - 1)
-//     game_word_index = 0;
-//   else
-//     game_word_index++;
-// }
-
-// function set_game()
-// {
-//   board = "";
-//   for (var i = 0; i < game_words[game_word_index].length; i++)
-//     board = board + "_";
-
-//   guesses_left = num_guesses;
-//   guesses = "";
-// }
-
-// function string_to_array(str)
-// {
-//   return(str.split('')); // empty string separator
-// }
-
-// function array_to_string(arr)
-// {
-//   return(arr.join(""));
-// }
-
-// set_game();
-// update_display();
-
-// document.onkeyup = function(event) 
-// {
-//   game_word_display.textContent = "";  // clear out winning word display
-
-//   var user_play = event.key;
-
-//   // if chars match insert into board variable
-//   var game_word_arr = string_to_array(game_words[game_word_index]);
-//   var board_arr = string_to_array(board);
-
-
-//   for (i = 0; i < game_word_arr.length; i++)
-//   {
-//     if (user_play === game_word_arr[i])
-//       board_arr[i] = user_play;
+// function createButtons() {
+//   for (var i = 0; i < 4; i++) {
+//       var button = $("<button>");
+//       button.text("button #" + (i + 1));
+//       button.addClass("number");
+//       button.attr("id", i + 1);
+//       $("#buttons").append(button);
 //   }
-
-//   board = array_to_string(board_arr);
-
-//   // determine win/loss/guess-again
-
-//   if (board === game_words[game_word_index]) //a win
-//   {
-//     wins++;
-//     game_word_display.textContent = game_words[game_word_index] + " - You won!"; // display winning word
-
-//     increment_game_word_index();
-//     set_game();
-    
-//   }
-//   else if (--guesses_left === 0)    // a loss
-//   {
-//     game_word_display.textContent = game_words[game_word_index] + " - You lost!"; // display losing word
-//     increment_game_word_index();
-//     set_game();
-
-//   }
-//   else                               //another guess
-//   {
-//     if ((guesses_left > 0) && (guesses_left < 8))
-//     {
-//       guesses = guesses + ', ';
-//     }
-//     guesses = guesses + user_play;
-//   }
-//   update_display();
 // }
 
 
-  
+// $("#number-to-guess").text(targetNumber);
+
+var numberOptions = [3, 9, 12, 2];
 
 
-    
+var reset_image = $("<img>");
+reset_image.addClass("reset");
+reset_image.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+$(".reset").append(reset_image);
+
+
+
+function create_crystals(){ 
+  for (var i = 0; i < numberOptions.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    $("#crystals").append(imageCrystal);
+  }
+}
+
+
+create_crystals();
+
+$(".reset").on("click", function() {
+  $("#crystals").empty();
+  alert("reset");
+  create_crystals();
+});
+
+
+// on-click event for button clicks of crystal images
+$("#crystals").on("click", ".crystal-image", function () {
+  var crystalValue = ($(this).attr("data-crystalvalue"));
+  crystalValue = parseInt(crystalValue);
+    // alert("You clicked a crystal!");
+  console.log(crystalValue);
+
+});
+
+
+
+
+
+
+// for (var i = 0; i < numberOptions.length; i++) {
+//   var imageCrystal = $("<img>");
+//   imageCrystal.addClass("crystal-image");
+//   imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+//   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+//   $("#crystals").append(imageCrystal);
+// }
+
+// for (var i = 0; i < numberOptions.length; i++) {
+//   var imageCrystal = $("<img>");
+//   imageCrystal.addClass("crystal-image");
+//   imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+//   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+//   $("#crystals").append(imageCrystal);
+// }
