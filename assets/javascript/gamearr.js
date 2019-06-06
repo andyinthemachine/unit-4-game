0cfd8e609492d3c42f296c250815b6d2e43dcf23
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-06-06 16:51:59 -0600

    fixed crystal images

--- assets/javascript/gamearr.js
+++ assets/javascript/gamearr.js
@@ -34,7 +34,7 @@ function init_crystals() {
   for (var i = 0; i < num_crystals; i++) {
     $images.push($("<img>"));
     $images[i].addClass("crystal-image");
-    $images[i].attr("src", "assets/images/crystal" + i + ".jpg");
+    $images[i].attr("src", "assets/images/crystal" + i + ".png");
     // $images[i].attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
     $("#crystals").append($images[i]);
   }
